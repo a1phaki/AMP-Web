c3ba4be80f1072acbe32d782649da8aad8e40504
a1phaki <jok.jok.875@gmail.com>
2025-01-20 15:15:32 +0800

    創建環境

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import axios from 'axios'
+import "./assets/style/style.scss";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
