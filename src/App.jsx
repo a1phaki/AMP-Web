f9137d4bf5b091cb7b28443902185ac73b144de9
a1phaki <jok.jok.875@gmail.com>
2025-01-22 00:03:29 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ModalForm from './component/modalForm';
 import Content from './component/content';
+import Header from './component/header';
 
 function App() {
   
 
   return (
     <>
+      <Header/>
       <Content/>
       <ModalForm/>
     </>
--- src/component/modalForm.jsx
+++ src/component/modalForm.jsx
@@ -3,9 +3,27 @@ import { useForm } from "react-hook-form";
 
 
 function ModalForm() {
-    const { register, handleSubmit, watch, reset } = useForm();
+    const { register, handleSubmit, watch, reset,setValue } = useForm();
     const [fastaContent, setFastaContent] = useState(""); // 儲存 fasta 檔案內容
     const [selectedOption, setSelectedOption] = useState(""); // 儲存 radio 選項
+
+    const [fileContent, setFileContent] = useState(""); // 用于存储文件内容
+
+    const handleFileChange = (event) => {
+      const file = event.target.files[0]; // 获取上传的文件
+  
+      if (file) {
+        const reader = new FileReader();
+  
+        reader.onload = (e) => {
+            console.log(e);
+          setFileContent(e.target.result); // 将文件内容存储到状态中
+          setValue("textarea", e.target.result); 
+        };
+  
+        reader.readAsText(file); // 以文本格式读取文件内容
+      }
+    };
 
     // 監聽 textarea 的輸入
     const textareaValue = watch("textarea", "");
@@ -63,7 +81,11 @@ function ModalForm() {
             activity:1.3292315355164965,
         }
     ];
-    
+
+    const handelOption = (e)=>{
+        setSelectedOption(e.target.value)
+    }
+
     // 記錄資料
     const handleRecordData = (data) => {
         if (!selectedOption) {
@@ -81,22 +103,31 @@ function ModalForm() {
             <div className='pt-3   pb-2 custom-border-top bg-secondary' >
                 <h2 className=' ps-3 fs-bold '>USE ANIA</h2>
             </div>
-            <div className='bg-white p-4'>
+            <div className='bg-white px-4 pt-1 pb-4'>
                 <form onSubmit={handleSubmit(handleRecordData)}>
                     {/* Textarea 與檔案選擇組 */}
-                    <div className="row border-bottom pb-5 my-3 border-2">
+                    <div className="row border-bottom pb-3 my-3 border-2">
                         <div className="col-6">
-                            <h3 className='text-primary fw-semibold mb-3'>Step1 <span className='text-black fw-normal h5'>You can paste a Protein sequence or several sequences with FASTA format into the field below</span></h3> 
+                            <div className="d-flex">
+                                <h4 className='text-primary fw-semibold mb-3 me-3'>Step1</h4>
+                                <h5 className="text-black fw-normal pt-1">You can paste a Protein sequence or several sequences with FASTA format into the field below</h5>
+                            </div>
                             <textarea
                                 {...register("textarea")}
-                                placeholder="請輸入內容..."
+                                placeholder={`# Enter your sequences in FASTA format:
+>DBAASP_5613
+AAAAAAAAAAGIGKFLHSAKKFGKAFVGEIMNS
+>DBAASP_11942
+AAAARRRR
+>DBAASP_13251
+AAARLRLLLYLITRR`}
                                 rows="12"
                                 style={{ width: "100%", marginBottom: "10px" }}
                                 className='form-control'
                             />
                         </div>
                         <div className="col-6">
-                            <h3 className='text-primary fw-semibold mb-5'><span className='text-black fw-normal h5'>OR Upload FASTA sequence file</span></h3>
+                            <h5 className='text-primary fw-semibold mb-5'><span className='text-black fw-normal h5'>OR Upload FASTA sequence file</span></h5>
                             <div className="custom-file">
                                 <h4 className='text-center pt-5'>Choose a file or drag & drop</h4>
                                 <div className='d-flex justify-content-center pt-3'>
@@ -107,57 +138,55 @@ function ModalForm() {
                                         id="fileInput"
                                         type="file"
                                         {...register("fileInput")}
+                                        onChange={handleFileChange} // 添加文件上传的 onChange 事件
                                         style={{ display: "none" }} // 隱藏原本的 file 按鈕
+                                        accept=".fasta" // 限制可上传的文件类型
                                     />
                                 </div>
                             </div>
                             <p className='pt-2 text-black-50' style={{'fontSize':'14px'}} >Upload a plain text file containing protein sequence(s) in FASTA format.</p>
                         </div>
-                        <div className="col-12 d-flex justify-content-end">
-                            <button
-                                type="button"
-                                onClick={() => handleGenerateFasta(watch())}
-                                disabled={!textareaValue} // 如果 textarea 無內容，按鈕禁用
-                                style={{
-                                    padding: "10px 20px",
-                                    backgroundColor: textareaValue ? "#F38986" : "#ccc",
-                                    cursor: textareaValue ? "pointer" : "not-allowed",
-                                }}
-                                className='btn text-white'
-                            >
-                                Get .fasta
-                            </button>
-                        </div>
                     </div>
-                    <div className="row pb-5 my-3 ">
+                    <div className="row pb-1 my-2 ">
                         <div className="col-6">
-                            <h3 className='text-primary fw-semibold'>Step2 <span className='text-black fw-normal h5'>Select type</span></h3>
-                            {ModalOption.map((option, index) => (
-                                <div className="form-check form-check-inline" key={index}>
-                                    <label style={{ display: "block", marginBottom: "10px" }} className='form-check-label'>
-                                        <input
-                                            type="radio"
-                                            value={option}
-                                            {...register("radioOption", {
-                                                required: true,
-                                                onChange: (e) => setSelectedOption(e.target.value),
-                                            })}
-                                            className='form-check-input'
-                                            name='modalType'
-                                        />
-                                        {option}
-                                    </label>
-                                </div>
-                            ))}
+                            <div className="d-flex">
+                                <h4 className='text-primary fw-semibold mb-3 me-3'>Step2</h4>
+                                <h5 className="text-black fw-normal pt-1">Select type</h5>
+                            </div>
+                            <div className="row">
+                                {ModalOption.map((option, index) => (
+                                    <div className='col-4' key={index}>
+                                        <div className={`custom-border custom-hover form-check border-1 py-2 border-primary border ${
+                                            selectedOption === option ? 'bg-info ' : ''}`} >
+                                            <label style={{ display: "block" }} className='form-check-label ms-2'>
+                                                <input
+                                                    type="radio"
+                                                    value={option}
+                                                    {...register("radioOption", {
+                                                        required: true,
+                                                    })}
+                                                    className='form-check-input '
+                                                    name='modalType'
+                                                    onClick={handelOption}
+                                                />
+                                                {option}
+                                            </label>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                         <div className="col-6">
-                            <h3 className='text-primary fw-semibold'>Step3 <span className='text-black fw-normal h5'>Input your project name</span></h3> 
+                            <div className="d-flex">
+                                <h4 className='text-primary fw-semibold mb-3 me-3'>Step3</h4>
+                                <h5 className="text-black fw-normal pt-1">Input your project name</h5>
+                            </div>
                             <input
                                 type="text"
                                 {...register("projectName", { required: false })}
                                 placeholder="Untitled Project"
-                                style={{ width: "100%", marginBottom: "10px" }}
-                                className='form-control mb-5'
+                                style={{ width: "100%" }}
+                                className='form-control py-2'
                             />
                         </div>
                     </div>
