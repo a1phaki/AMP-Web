2a66bde015259097d777403b3280ab3ddf690ed6
a1phaki <jok.jok.875@gmail.com>
2025-01-21 12:25:16 +0800

    完成內容區塊

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ModalForm from './component/modalForm';
+import Content from './component/content';
 
 function App() {
   
 
   return (
     <>
+      <Content/>
       <ModalForm/>
     </>
   )
--- src/component/content.jsx
+++ src/component/content.jsx
@@ -1,5 +1,32 @@
 function Content() {
-    return ()
+    return (
+        <>
+            <div className="container bg-success px-3 py-5">
+                <div className="bg-white custom-border">
+                    <div className="d-flex justify-content-center py-5">
+                        <img src="https://images.unsplash.com/photo-1641903202531-bfa6bf0c6419?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2VxdWVuY2UlMjBnZW5lfGVufDB8fDB8fHww" className="object-fit-cover px-5" width='100%' alt="主圖" />
+                    </div>
+                    <h6 className="pb-5 px-5">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eveniet corporis unde a aliquid ipsam sed nisi dolore numquam, quaerat exercitationem laudantium quae animi, aperiam enim. Sequi, nobis veritatis. Ea.
+                        Perspiciatis, reiciendis quos dolores sequi, ex perferendis quibusdam laborum similique, aperiam laboriosam aliquam explicabo sunt maxime veritatis natus necessitatibus deleniti nostrum. Ab asperiores, error esse similique suscipit minus nulla at.
+                        Sequi suscipit ullam nobis alias hic delectus sed placeat, labore commodi harum? Eaque animi iusto laboriosam, quo rerum, sequi exercitationem quos labore explicabo natus deleniti porro quasi nisi neque voluptates?
+                        Aut sit vel maxime accusantium reprehenderit, soluta iusto nihil deleniti error, tenetur magni fugiat! Iste, sit, perspiciatis accusantium repudiandae maxime amet iusto suscipit fugit nulla fuga laboriosam aspernatur quia neque.
+                        Facilis sit accusantium quisquam eaque suscipit alias, qui rem cumque quibusdam distinctio animi iste nihil deleniti! Fuga quidem hic voluptatibus consequatur corrupti cupiditate vel illum quas, aliquam autem sit temporibus?
+                        Eos minima consectetur nostrum ex blanditiis accusamus nam fugit soluta officia, deleniti fugiat perferendis exercitationem dolorum ducimus amet assumenda animi ipsa. Quisquam sequi nisi, deserunt cupiditate ut dolor nam tenetur.
+                        Cupiditate aliquam esse, ratione soluta nesciunt impedit consequuntur unde! Iusto sunt blanditiis quibusdam distinctio eius, at nisi modi placeat dolore fugiat dolores optio non voluptatibus fuga consequatur explicabo asperiores nemo!
+                        Iure cum totam libero esse laboriosam nobis, ex, obcaecati quibusdam blanditiis quaerat dolore repudiandae provident hic perspiciatis quos ullam neque ducimus? Adipisci impedit eum quaerat ratione corrupti, dignissimos laborum quas.
+                        Molestiae eum ullam minima laudantium laboriosam at porro sapiente repellendus voluptate quod, quas molestias illum obcaecati. Delectus id perferendis quaerat excepturi molestias in, aliquam voluptatibus totam itaque, iste magnam debitis.
+                        Corrupti labore omnis quisquam ducimus atque ipsam adipisci voluptate quas, dignissimos accusamus eaque, necessitatibus eos sequi rem sapiente nisi culpa laudantium tempora obcaecati inventore repellendus. Id voluptatum dicta ipsam totam.
+                        In repellat accusantium enim eius aperiam magni. Qui nisi, nostrum magni mollitia sapiente dignissimos id laudantium praesentium cum placeat alias temporibus enim cumque tenetur. Omnis quidem quam nostrum sed dolorem.
+                        Cum voluptas suscipit fugit assumenda quaerat maiores? Eligendi exercitationem itaque soluta facere in reprehenderit repudiandae rerum ipsum fuga, delectus mollitia, magnam, deserunt ipsa ea non provident pariatur nostrum tenetur maiores.
+                        Facilis, ea explicabo? Veritatis consequuntur, in facere quisquam voluptatem porro, asperiores quaerat minima dicta necessitatibus recusandae. Tempora error exercitationem quibusdam nisi, nesciunt quas? Velit, corrupti necessitatibus numquam cumque aliquam dolores.
+                        Corporis sed voluptas atque provident sint, ducimus molestiae consectetur alias odio a quis tempore soluta harum explicabo laboriosam quibusdam animi laborum mollitia tenetur voluptates dolor? Nemo tempora aliquam sunt illo.
+                        Sint amet laboriosam eius nihil quam sit harum laborum totam repudiandae accusantium id cupiditate, soluta earum aliquam officia perspiciatis cum maiores? Natus in sequi iusto commodi harum beatae temporibus impedit?
+                    </h6>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Content
