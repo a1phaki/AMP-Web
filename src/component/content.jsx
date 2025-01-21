4e66c9f5d0093bcbb00c2f864e4ed3eb973eb82f
a1phaki <jok.jok.875@gmail.com>
2025-01-22 02:42:43 +0800

    更新圖片路徑

--- src/component/content.jsx
+++ src/component/content.jsx
@@ -4,7 +4,7 @@ function Content() {
             <div className="container bg-success px-3 py-5">
                 <div className="bg-white custom-border">
                     <div className="d-flex justify-content-center py-5">
-                        <img src="./img/Workflow.png" className="object-fit-cover px-5" width='100%' alt="主圖" />
+                        <img src="/img/Workflow.png" className="object-fit-cover px-5" width='100%' alt="主圖" />
                     </div>
                     <h4 className="pb-5 px-5">
                     The overuse of antibiotics has led to significant microbial resistance, necessitating alternative therapies. Antimicrobial peptides (AMPs) are small proteins effective against various pathogens, making them promising candidates for new anti-infective drugs. This study proposed an encoding method for AMP sequences using Chaos Game Representation (CGR) and protein language model embeddings. These methods converted the sequences into frequency matrices resembling image features. These matrices served as input for Convolutional Neural Networks (CNNs) with Inception Modules to capture local features. The extracted features were then processed by a Transformer model to predict MIC values for Staphylococcus aureus, Escherichia coli, and Pseudomonas aeruginosa.
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,14 +1,9 @@
-
-
-
-
-
 function Header() {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-white">
                     <div className="container-fluid ">
-                      <a className="navbar-brand" href="#"><h1 className="mb-0"><img src="./public/img/e3dd88d8bc9492f1f0a1ee5a4d76bbc9.png" alt="標題" className="img-title"/></h1></a>
+                      <a className="navbar-brand" href="#"><h1 className="mb-0"><img src="/img/e3dd88d8bc9492f1f0a1ee5a4d76bbc9.png" alt="標題" className="img-title"/></h1></a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
