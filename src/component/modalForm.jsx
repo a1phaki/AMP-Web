2e8a90b66ecafb40114f7d4d648b2ec9a3b00b1d
a1phaki <jok.jok.875@gmail.com>
2025-01-25 02:05:04 +0800

    step1排版，處理表單送出功能

--- src/component/modalForm.jsx
+++ src/component/modalForm.jsx
@@ -10,7 +10,9 @@ function ModalForm() {
         fileName:'',
         fileSize:'',
         fileContent:''
-    })
+    });
+
+    const [isUseANIA,setIsUseANIA] = useState(false);
     const [isSelected, setIsSelected] = useState(false);
     const [fileContent, setFileContent] = useState(""); // 用于存储文件内容
 
@@ -54,43 +56,31 @@ function ModalForm() {
         'P. aerugionsa',
     ]
 
-    const tableData = [
-        {
-            id:'DBAASP_17697',
-            sequence:'WRKWRKWRK',
-            target:'E.coli',
-            activity:0.9103199341229964,
-        },
-        {
-            id:'DBAASP_14420',
-            sequence:'KILPGVCKKIMRPFLRRISKDILTGKK',
-            target:'E.coli',
-            activity:0.7524759784904157,
-        },
-        {
-            id:'DBAASP_2424',
-            sequence:'GIFNVFKGALKTAGKHVAGSLLNQLKCKVSGEC',
-            target:'E.coli',
-            activity:1.1181126138837496,
-        },
-        {
-            id:'DBAASP_17661',
-            sequence:'GLLKRWKTLL',
-            target:'E.coli',
-            activity:1.3455250424357494,
-        },
-        {
-            id:'DBAASP_1618',
-            sequence:"GFFDRIKALTKNVTLELLNTITGKLGVTGG",
-            target:'E.coli',
-            activity:1.3292315355164965,
+    const [tableData,setTableData] = useState([{
+            id:'',
+            sequence:'',
+            target:'',
+            activity:''
         }
-    ];
+    ]);
 
     const handelOption = (e)=>{
         setSelectedOption(e.target.value)
     }
 
+    const handleFormSubmit = (data)=>{
+        const lines = data.fastaData.trim().split("\n");
+        const result = [];
+
+        for (let i = 0; i < lines.length; i += 2) {
+            const id = lines[i].replace(">", "").trim();
+            const sequence = lines[i + 1].trim();
+            result.push({ id, sequence,target:data.target });
+        }
+
+        setTableData(result);
+        setIsUseANIA(true);
+    }
   return (
     <>
         <div className='container py-5 bg-success'>
@@ -98,16 +88,17 @@ function ModalForm() {
                 <h2 className=' ps-3 fs-bold h5'>USE ANIA</h2>
             </div>
             <div className='bg-white px-4 pt-1 pb-4'>
-                <form onSubmit={handleSubmit()}>
+                <form onSubmit={handleSubmit(handleFormSubmit)}>
                     {/* Textarea 與檔案選擇組 */}
-                    <div className="row border-bottom pb-3 my-3 border-2">
+                    <div className="row border-bottom my-3 border-2">
+                        <div className="col-12 d-flex">
+                            <h4 className='text-primary fw-semibold mb-3 me-3'>Step1</h4>
+                            <h5 className="text-black fw-normal pt-1">You can paste a Protein sequence or several sequences with FASTA format into the field below</h5>
+                        </div>
+
                         <div className="col-6">
-                            <div className="d-flex">
-                                <h4 className='text-primary fw-semibold mb-3 me-3'>Step1</h4>
-                                <h5 className="text-black fw-normal pt-1">You can paste a Protein sequence or several sequences with FASTA format into the field below</h5>
-                            </div>
                             <textarea
-                                {...register("textarea")}
+                                {...register("fastaData")}
                                 placeholder={`# Enter your sequences in FASTA format:
 >DBAASP_5613
 AAAAAAAAAAGIGKFLHSAKKFGKAFVGEIMNS
@@ -120,40 +111,44 @@ AAARLRLLLYLITRR`}
                                 className='form-control'
                             />
                         </div>
-                        <div className="col-6">
-                            <h5 className='text-primary fw-semibold mb-5'><span className='text-black fw-normal h5'>OR Upload FASTA sequence file</span></h5>
-                            <div className="custom-file py-4">
-                                <h6 className='text-center pb-3'>Choose a file or drag & drop</h6>
-                                <div className='d-flex justify-content-center '>
-                                    <label htmlFor="fileInput" className="text-center h4 btn btn-outline-primary btn-lg">
-                                        Choose a file 
-                                    </label>
-                                    <input
-                                        id="fileInput"
-                                        type="file"
-                                        {...register("fileInput")}
-                                        onChange={handleFileChange} // 添加文件上传的 onChange 事件
-                                        style={{ display: "none" }} // 隱藏原本的 file 按鈕
-                                        accept=".fasta" // 限制可上传的文件类型
-                                    />
-                                </div>
+                        <div className="row col-6">
+                            <div className="col-1 d-flex align-items-center">
+                                <h4>or</h4>
                             </div>
-                            <div className="custom-select-file my-7 d-flex align-items-center">
-                                <div className="custom-border border-1 border-danger border ms-7">
-                                    <InsertDriveFileIcon className="m-3" sx={{ color: '#F38986' }}/>
-                                </div>
-                                {isSelected?
-                                    <div className="ps-3 file-info">
-                                        <h4 className="mb-0 pt-3">{fileData.fileName}</h4>
-                                        <p className="">{`${fileData.fileSize} bytes`}</p> 
+                            <div className="col-11">
+                                <div className="custom-file py-5">
+                                    <h6 className='text-center pb-4'>Choose a file or drag & drop</h6>
+                                    <div className='d-flex justify-content-center '>
+                                        <label htmlFor="fileInput" className="text-center h4 btn btn-outline-primary btn-lg">
+                                            Choose a file 
+                                        </label>
+                                        <input
+                                            id="fileInput"
+                                            type="file"
+                                            {...register("fileInput")}
+                                            onChange={handleFileChange} // 添加文件上传的 onChange 事件
+                                            style={{ display: "none" }} // 隱藏原本的 file 按鈕
+                                            accept=".fasta" // 限制可上传的文件类型
+                                        />
                                     </div>
-                                :
-                                    <div className="ps-3 file-info">    
-                                        <h4 className="mb-0">No file selected</h4>
-                                    </div>    
-                                }
+                                </div>
+                                <div className="custom-select-file my-3 d-flex align-items-center ">
+                                    <div className="custom-border border-1 border-danger border ms-7">
+                                        <InsertDriveFileIcon className="m-3" sx={{ color: '#F38986' }}/>
+                                    </div>
+                                    {isSelected?
+                                        <div className="ps-3 file-info">
+                                            <h4 className="mb-0 pt-3">{fileData.fileName}</h4>
+                                            <p className="">{`${fileData.fileSize} bytes`}</p> 
+                                        </div>
+                                    :
+                                        <div className="ps-3 file-info">    
+                                            <h4 className="mb-0">No file selected</h4>
+                                        </div>    
+                                    }
+                                </div>
+                                <p className=' text-black-50' style={{'fontSize':'14px'}} >Upload a plain text file containing protein sequence(s) in FASTA format.</p>
                             </div>
-                            <p className='pt-2 text-black-50' style={{'fontSize':'14px'}} >Upload a plain text file containing protein sequence(s) in FASTA format.</p>
                         </div>
                     </div>
                     <div className="row pb-1 my-2 ">
@@ -171,12 +166,12 @@ AAARLRLLLYLITRR`}
                                                 <input
                                                     type="radio"
                                                     value={option}
-                                                    {...register("radioOption", {
+                                                    {...register("target", {
                                                         required: true,
+                                                        onChange:(e)=>handelOption(e)
                                                     })}
                                                     className='form-check-input '
-                                                    name='modalType'
-                                                    onClick={handelOption}
+                                                    name='target'
                                                 />
                                                 {option}
                                             </label>
@@ -199,50 +194,55 @@ AAARLRLLLYLITRR`}
                             />
                         </div>
                     </div>
+                    <div className="row " style={{ backgroundColor: '#F9FAFB' }}>
+                        <div className="col p-3 d-flex justify-content-end custom-border-bottom">
+                        <button type='button' className='btn btn-outline-primary btn-lg me-4' onClick={clearFields}>
+                                Clear fields
+                            </button>
+                            <button type="submit" className='btn btn-primary btn-lg text-white'>
+                                Start ANIA
+                            </button>
+                        </div>
+                    </div>
                 </form>
             </div>
-            <div className='p-3 d-flex justify-content-end custom-border-bottom' style={{ backgroundColor: '#F9FAFB' }}>
-                <button type='button' className='btn btn-outline-primary btn-lg me-4' onClick={clearFields}>
-                    Clear fields
-                </button>
-                <button type="submit" className='btn btn-primary btn-lg text-white'>
-                    Start ANIA
-                </button>
-            </div>
         </div>
-
-        <div className='container py-5 bg-success'>
-            <div className='pt-3   pb-2 custom-border-top bg-secondary' >
-                <h2 className='ps-3 fs-bold h5'>RESULT TABLE</h2>
-            </div>
-            <div className='bg-white p-4'>
-                <table className='table-secondary table table-striped'>
-                    <thead>
-                        <tr>
-                            <th scope="col" width='15%'>ID</th>
-                            <th scope="col" width='35%'>Sequence</th>
-                            <th scope="col" width='15%'>Target</th>
-                            <th scope="col" width='35%'>Activity (log MIC, unit: uM)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tableData.map((data,index)=>(
-                            <tr key={index}>
-                                <td scope="row">{data.id}</td>
-                                <td>{data.sequence}</td>
-                                <td>{data.target}</td>
-                                <td>{data.activity}</td>
+        {
+            isUseANIA &&
+            (<div className='container py-5 bg-success'>
+                <div className='pt-3   pb-2 custom-border-top bg-secondary' >
+                    <h2 className='ps-3 fs-bold h5'>RESULT TABLE</h2>
+                </div>
+                <div className='bg-white p-4'>
+                    <table className='table-secondary table table-striped'>
+                        <thead>
+                            <tr>
+                                <th scope="col" width='15%'>ID</th>
+                                <th scope="col" width='35%'>Sequence</th>
+                                <th scope="col" width='15%'>Target</th>
+                                <th scope="col" width='35%'>Activity (log MIC, unit: uM)</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className='p-3 d-flex justify-content-end custom-border-bottom' style={{ backgroundColor: '#F9FAFB' }}>
-                <button type="submit" className='btn btn-primary btn-lg text-white'>
-                    Export .csv
-                </button>
-            </div>
-        </div>
+                        </thead>
+                        <tbody>
+                            {tableData.map((data,index)=>(
+                                <tr key={index}>
+                                    <td scope="row">{data.id}</td>
+                                    <td>{data.sequence}</td>
+                                    <td>{data.target}</td>
+                                    <td>{data.activity}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className='p-3 d-flex justify-content-end custom-border-bottom' style={{ backgroundColor: '#F9FAFB' }}>
+                    <button type="submit" className='btn btn-primary btn-lg text-white'>
+                        Export .csv
+                    </button>
+                </div>
+            </div>)
+        }
+        
     </>
     
   );
